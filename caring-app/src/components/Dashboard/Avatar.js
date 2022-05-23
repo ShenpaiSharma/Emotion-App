39f16a6015febb91d5aee434badb71937c282b97
Shubham Kumar <47415702+ShenpaiSharma@users.noreply.github.com>
2022-05-23 17:32:06 +0530

    Delete caring-app directory

--- caring-app/src/components/Dashboard/Avatar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Card.css";
-
-function Avatar(props) {
-  return <img className="circle-img" src={props.imgsrc} alt="avatar_img" />;
-}
-
-export default Avatar;
